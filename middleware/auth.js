c33d993326c9295e9d182b1c2c1410ffc16928b9
V Satwik Reddy <satwikstudies05@gmail.com>
2025-03-07 23:50:19 +0530

    added middle ware and home.js(a protected route)

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,12 +3,13 @@ const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
     let token = req.cookies.token || req.header("Authorization");
 
-    if (!token) return res.status(401).json({ message: "Access denied. No token provided." });
+    if (!token) return res.status(401).json({ message: "No token provided." });
 
     try {
         const extractedToken = token.startsWith("Bearer ") ? token.split(" ")[1] : token;
         const decoded = jwt.verify(extractedToken, process.env.JWT_SECRET);
         req.user = decoded;
+        console.log(extractedToken)
         next();
     } catch (error) {
         res.status(400).json({ message: "Invalid token" });
--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", auth, (req, res) => {
-    res.json({ message: "Welcome to the protected route!", userId: req.user.id });
+    res.json({ message: "Welcome to the protected route!", userId: req.user.username });
 });
 
 module.exports = router;
