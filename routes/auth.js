efcf6db365f07008ae89f807a5166d19c20d944b
V Satwik Reddy <satwikstudies05@gmail.com>
2025-03-23 15:32:00 +0530

    trying cros origin cookie sent

--- routes/auth.js
+++ routes/auth.js
@@ -53,8 +53,8 @@ router.post("/login", async (req, res) => {
         
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false, 
-            sameSite: "Strict",
+            secure: true, 
+            sameSite: "None",
             maxAge: 24 * 60 * 60 * 1000, 
         });
         const tasks=await Task.find({user:user._id});
