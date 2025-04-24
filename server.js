d6d39a880180638266dc6508ecc9d4e971e4bf04
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-04-24 23:11:28 +0530

    cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 //  Improved CORS setup for Cookies
 app.use(
     cors({
-        origin: ["https://v-satwik-reddy.github.io"],
+        origin: ["http://localhost:3000","https://v-satwik-reddy.github.io"],
         credentials: true, // **Ensures frontend can send & receive cookies**
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"]
